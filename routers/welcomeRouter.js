831864193f6b42e039de0c18b559907525488789
Lullaby16 <samuelys945@gmail.com>
2023-01-15 11:04:57 +0700

    update server cors

--- routers/welcomeRouter.js
+++ routers/welcomeRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 require("dotenv").config();
 
 router.get("/", (req, res) => {
-  res.json("Welcome to my server ", process.env.COMMENT);
+  res.json(`Welcome to my server ${process.env.COMMENT}`);
 });
 
 module.exports = router;
